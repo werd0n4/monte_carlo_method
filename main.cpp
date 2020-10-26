89dd055fcd59200d0dcfd06569baf0a642e01b97
Filip Gajewski <filipgajewski4@gmail.com>
2020-10-26 17:08:42 +0100

    Small addon

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <curand.h>
 #include <curand_kernel.h>
-#include "sequence/monte_carlo.cpp"
+#include "sequence/monte_carlo.hpp"
 
 
 int main()
 {
     cudaDeviceSynchronize();
+    cudaDeviceReset();
     return 0;
 }
